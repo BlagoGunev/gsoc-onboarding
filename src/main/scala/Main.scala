9ef6063e9ff011b810f257ee5c5471d967eb8b50
Antonio Jimenez <antonio.jimenez.nieto@gmail.com>
2026-02-25 18:00:00 +0100

    move Validation logic to a separete file

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -8,34 +8,13 @@ import calico.unsafe.given
 
 import fs2.concurrent.*
 import fs2.dom.HtmlElement
-
-import io.circe.*
-import io.circe.syntax.*
-
-import org.http4s.*
-import org.http4s.circe.*
-import org.http4s.dom.*
-import org.http4s.implicits.uri
-import org.http4s.circe.CirceEntityCodec.circeEntityDecoder
-
-import org.typelevel.ci.CIStringSyntax
+import org.http4s.dom.FetchClientBuilder
 
 val contributors: List[(String, Resource[IO, HtmlElement[IO]])] = List(
   "antoniojimenez" -> AntonioJimenez.component
 )
 
-final case class OutOfOrder(shouldBeBefore: String, butIsAfter: String) derives Decoder
-final case class ValidationResponse(valid: Boolean, outOfOrder: Option[OutOfOrder])
-    derives Decoder
-
 object Main extends IOWebApp:
-  val validationRequest =
-    val body = contributors.map(_._1).asJson.noSpaces
-
-    Request[IO](Method.POST, uri"https://cloudflare-typelevel.org")
-      .withEntity(body)
-      .putHeaders(Header.Raw(ci"Content-Type", "application/json"))
-
   def render: Resource[IO, HtmlElement[IO]] =
     val handles = contributors.map(_._1)
     val client = FetchClientBuilder[IO].create
@@ -43,21 +22,7 @@ object Main extends IOWebApp:
     SignallingRef[IO].of[Option[String]](None).toResource.flatMap { validationMsg =>
 
       val validate: IO[Unit] =
-        client
-          .expect[ValidationResponse](validationRequest)
-          .attempt
-          .flatMap:
-            case Right(ValidationResponse(true, _)) =>
-              validationMsg.set(None)
-
-            case Right(ValidationResponse(_, Some(OutOfOrder(before, after)))) =>
-              validationMsg.set(Some(s"$before should be before $after"))
-
-            case Right(_) =>
-              validationMsg.set(Some("Order is invalid"))
-
-            case Left(_) =>
-              validationMsg.set(Some("Could not reach validation API"))
+        Validation.validate(handles, client).flatMap(validationMsg.set)
 
       Resource.eval(validate) *>
         div(
@@ -74,9 +39,7 @@ object Main extends IOWebApp:
             onClick --> (_.foreach(_ => validate)),
             "Verify order"
           ),
-          ul(
-            contributors.map { (handle, _) => li(a(href := s"#$handle", s"@$handle")) }
-          ),
+          ul(contributors.map { (handle, _) => li(a(href := s"#$handle", s"@$handle")) }),
           contributors.map { (handle, component) => div(idAttr := handle, component) }
         )
     }
---
+++ src/main/scala/Validation.scala
@@ -0,0 +1,36 @@
+import cats.effect.*
+
+import fs2.concurrent.*
+
+import io.circe.*
+import io.circe.syntax.*
+
+import org.http4s.*
+import org.http4s.client.Client
+import org.http4s.circe.CirceEntityCodec.circeEntityDecoder
+import org.http4s.dom.*
+
+import org.typelevel.ci.CIStringSyntax
+
+final case class OutOfOrder(shouldBeBefore: String, butIsAfter: String) derives Decoder
+final case class ValidationResponse(valid: Boolean, outOfOrder: Option[OutOfOrder])
+    derives Decoder
+
+object Validation:
+  val workerUrl = "https://cloudflare-typelevel.org"
+
+  def request(handles: List[String]): Request[IO] =
+    Request[IO](Method.POST, Uri.unsafeFromString(workerUrl))
+      .withEntity(handles.asJson.noSpaces)
+      .putHeaders(Header.Raw(ci"Content-Type", "application/json"))
+
+  def validate(handles: List[String], client: Client[IO]): IO[Option[String]] =
+    client
+      .expect[ValidationResponse](request(handles))
+      .attempt
+      .map:
+        case Right(ValidationResponse(true, _)) => None
+        case Right(ValidationResponse(_, Some(OutOfOrder(before, after)))) =>
+          Some(s"$before should be before $after")
+        case Right(_) => Some("Order is invalid")
+        case Left(_) => Some("Could not reach validation API")
