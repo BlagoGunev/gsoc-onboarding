418987989bbc16fc94d5336c7f1afb56551dc9c1
abby-ql <abby@THINKPADT480S.localdomain>
2026-02-27 16:53:47 +0000

    correct formating/linting

--- src/main/scala/gsoc/contributors/abby_ql.scala
+++ src/main/scala/gsoc/contributors/abby_ql.scala
@@ -14,30 +14,32 @@ val abby_ql: Contributor = Contributor("abby-ql"):
     div(
       p(
         "Hi I'm ",
-        revealed.map {
-          case false => "black"
-          case true  => "rainbow"
-        }.changes.map { mode =>
-          if mode == "black" then
-            span(
-              styleAttr := "font-weight: bold;",
-              "@abby-ql"
-            )
-          else
-            span(
-              span(styleAttr := "color: violet; font-weight: bold;", "@"),
-              span(styleAttr := "color: red; font-weight: bold;", "a"),
-              span(styleAttr := "color: red; font-weight: bold;", "b"),
-              span(styleAttr := "color: orange; font-weight: bold;", "b"),
-              span(styleAttr := "color: orange; font-weight: bold;", "y"),
-              span(styleAttr := "color: yellow; font-weight: bold;", "-"),
-              span(styleAttr := "color: green; font-weight: bold;", "q"),
-              span(styleAttr := "color: blue; font-weight: bold;", "l")
-            )
-        },
+        revealed
+          .map {
+            case false => "black"
+            case true => "rainbow"
+          }
+          .changes
+          .map { mode =>
+            if mode == "black" then
+              span(
+                styleAttr := "font-weight: bold;",
+                "@abby-ql"
+              )
+            else
+              span(
+                span(styleAttr := "color: violet; font-weight: bold;", "@"),
+                span(styleAttr := "color: red; font-weight: bold;", "a"),
+                span(styleAttr := "color: red; font-weight: bold;", "b"),
+                span(styleAttr := "color: orange; font-weight: bold;", "b"),
+                span(styleAttr := "color: orange; font-weight: bold;", "y"),
+                span(styleAttr := "color: yellow; font-weight: bold;", "-"),
+                span(styleAttr := "color: green; font-weight: bold;", "q"),
+                span(styleAttr := "color: blue; font-weight: bold;", "l")
+              )
+          },
         " on GitHub. I agree to follow the Typelevel CoC and GSoC AI policy."
       ),
-
       revealed.map(r =>
         if r then
           p(
@@ -49,15 +51,12 @@ val abby_ql: Contributor = Contributor("abby-ql"):
             span(styleAttr := "color: indigo; font-weight: bold;", "to "),
             span(styleAttr := "color: violet; font-weight: bold;", "Typelevel!")
           )
-        else div("")
-      ),
-
+        else div("")),
       button(
         onClick --> (_.foreach(_ => revealed.update(!_))),
         revealed.map(r =>
           if r then "Hide rainbow :)"
-          else "Click to reveal rainbow :)"
-        )
+          else "Click to reveal rainbow :)")
       )
     )
   }
